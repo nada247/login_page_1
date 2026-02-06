ada721ded992f569b8fb820f98780f21f26681a6
Nada Mohamed <nadabdulrahman@students.du.edu.eg>
2026-02-06 05:01:36 +0200

    add

---
+++ README.md
@@ -0,0 +1,3 @@
+# login_page_1
+
+![Home Screen](assets/screen1.jpg)
---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:login_page_1/animation.dart';
+
+void main() => runApp(
+    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage()
+    )
+);
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: 400,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: -40,
+                    height: 400,
+                    width: width,
+                    child: FadeAnimation(1, Container(
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/background.png'),
+                              fit: BoxFit.fill
+                          )
+                      ),
+                    )),
+                  ),
+                  Positioned(
+                    height: 400,
+                    width: width+20,
+                    child: FadeAnimation(1, Container(
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/background-2.png'),
+                              fit: BoxFit.fill
+                          )
+                      ),
+                    )),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeAnimation(1,Text("Login", style: TextStyle(color: Color.fromRGBO(49, 39, 79, 1), fontWeight: FontWeight.bold, fontSize: 30),)),
+                  SizedBox(height: 30,),
+                  FadeAnimation(1, Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        border: Border.all(color: Color.fromRGBO(196, 135, 198, .3)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromRGBO(196, 135, 198, .3),
+                            blurRadius: 20,
+                            offset: Offset(0, 10),
+                          )
+                        ]
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              border: Border(bottom: BorderSide(
+                                  color: Color.fromRGBO(196, 135, 198, .3)
+                              ))
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Username",
+                                hintStyle: TextStyle(color: Colors.grey.shade700)
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Password",
+                                hintStyle: TextStyle(color: Colors.grey.shade700)
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )),
+                  SizedBox(height: 20,),
+                  FadeAnimation(1, Center(child: TextButton(onPressed: () {}, child: Text("Forgot Password?", style: TextStyle(color: Color.fromRGBO(196, 135, 198, 1)),)))),
+                  SizedBox(height: 30,),
+                  FadeAnimation(1.2, MaterialButton(
+                    onPressed: () {},
+                    color: Color.fromRGBO(49, 39, 79, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    height: 50,
+                    child: Center(
+                      child: Text("Login", style: TextStyle(color: Colors.white),),
+                    ),
+                  )),
+                  SizedBox(height: 30,),
+                  FadeAnimation(1.3, Center(child: TextButton(onPressed: () {}, child: Text("Create Account", style: TextStyle(color: Color.fromRGBO(49, 39, 79, .6)),)))),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
